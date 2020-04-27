29b2bbb8dec6b766e4863b612a19d5857d9e50b8
sergiuser1 <serge.ojog@gmail.com>
2020-04-27 15:15:25 +0200

    Refactor long lines and uppercase letters in functions
    Add no useful code whatsoever, but at least it looks pretty

--- main.py
+++ main.py
@@ -10,8 +10,8 @@ from telegram.ext import Updater, CommandHandler
 import string
 
 from currency import Currency
-from api_access import get_all_prices_from_API, get_price_from_API_for, \
-    get_all_prices_from_API_for_date
+from api_access import get_all_prices_from_api, get_price_from_api_for, \
+    get_all_prices_from_api_for_date
 
 
 BOT_TOKEN = os.environ['BOT_TOKEN']
@@ -39,22 +39,24 @@ def run_threaded(job_function):  # run every schedule on different threads
 
 
 def verify_price():
-    curr_prices = get_all_prices_from_API()
-    yesterday_prices = get_all_prices_from_API_for_date(
+    curr_prices = get_all_prices_from_api()
+    yesterday_prices = get_all_prices_from_api_for_date(
         search_date=datetime.today() - timedelta(days=1))
 
     print("verified euro for today= {} & yesterday= {}".format(
         curr_prices[Currency.EURO], yesterday_prices[Currency.EURO]))
 
-    if abs(curr_prices[Currency.EURO] - yesterday_prices[Currency.EURO]) >= 0.2:
-        send_message("wtf, man, euro is: {}, just yesterday it was: {}".format(
+    if abs(curr_prices[Currency.EURO]
+           - yesterday_prices[Currency.EURO]) >= 0.2:
+        send_message("Wtf, man, an euro is: {}, just yesterday it was: {}".format(
             curr_prices[Currency.EURO], yesterday_prices[Currency.EURO]))
 
     print("verified US_DOLLAR for today= {} & yesterday= {}".format(
         curr_prices[Currency.US_DOLLAR], yesterday_prices[Currency.US_DOLLAR]))
 
-    if abs(curr_prices[Currency.US_DOLLAR] - yesterday_prices[Currency.US_DOLLAR]) >= 0.2:
-        send_message("wtf, man, US_DOLLAR is: {}, just yesterday it was: {}".format(
+    if abs(curr_prices[Currency.US_DOLLAR]
+           - yesterday_prices[Currency.US_DOLLAR]) >= 0.2:
+        send_message("Wtf, man, a dollar is: {}, just yesterday it was: {}".format(
             curr_prices[Currency.US_DOLLAR], yesterday_prices[Currency.US_DOLLAR]))
 
 
@@ -65,10 +67,10 @@ def send_message(message: string):
 
 
 def send_all_prices_message():
-    curr_prices = get_all_prices_from_API()
+    curr_prices = get_all_prices_from_api()
     bot = telegram.Bot(token=BOT_TOKEN)
     try:
-        bot.sendMessage(chat_id=CHAT_ID, text="1€ = {}mdl, 1$ = {]mdl".format(
+        bot.sendMessage(chat_id=CHAT_ID, text="1€ = {}mdl, 1$ = {}mdl".format(
             curr_prices[Currency.EURO], curr_prices[Currency.US_DOLLAR]))
     except ConnectionError:
         bot.sendMessage(chat_id=CHAT_ID, text="Currency service unavailable")
@@ -85,7 +87,7 @@ def handle_euro_command(update, context):
     try:
         context.bot.sendMessage(
             chat_id=update.effective_chat.id,
-            text="1€ = {}mdl".format(get_price_from_API_for(Currency.EURO)))
+            text="1€ = {}mdl".format(get_price_from_api_for(Currency.EURO)))
     except ConnectionError:
         context.bot.sendMessage(chat_id=update.effective_chat.id,
                                 text="Currency service unavailable")
@@ -96,14 +98,14 @@ def handle_dollar_command(update, context):
     try:
         context.bot.sendMessage(
             chat_id=update.effective_chat.id,
-            text="1€ = {}mdl".format(get_price_from_API_for(Currency.US_DOLLAR)))
+            text="1€ = {}mdl".format(get_price_from_api_for(Currency.US_DOLLAR)))
     except ConnectionError:
         context.bot.sendMessage(chat_id=update.effective_chat.id,
                                 text="Currency service unavailable")
 
 def handle_all_command(update, context):
     print("handling command: /all")
-    curr_prices = get_all_prices_from_API()
+    curr_prices = get_all_prices_from_api()
     try:
         context.bot.sendMessage(
             chat_id=update.effective_chat.id,
